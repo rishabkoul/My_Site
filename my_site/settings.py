093980a7ac65316d532fefcc275b58105131fa3b
Rishab Koul <rishabkoul2001@gmail.com>
2020-10-07 12:18:35 +0530

    intial-commit

--- my_site/settings.py
+++ my_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'w)fb8&*6tdh4cm3nr&o)(0z2835s2+116$0^u1tn-f^0b&b-va'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static1'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
